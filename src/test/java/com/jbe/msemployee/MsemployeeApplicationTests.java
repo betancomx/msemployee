9d73c749ef840382229fdf36c102df830349ea5b
betancomx <betancomx@hotmail.com>
2026-04-15 09:36:42 -0600

    Initial upload msemployee

---
+++ src/test/java/com/jbe/msemployee/MsemployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbe.msemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsemployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
